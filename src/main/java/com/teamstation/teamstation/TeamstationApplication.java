cf4d65950b4cf36410804878a3beff1bb6706e2a
jhsseonn <hs125bt@gmail.com>
2023-06-28 22:35:40 +0900

    project upload

---
+++ src/main/java/com/teamstation/teamstation/TeamstationApplication.java
@@ -0,0 +1,13 @@
+package com.teamstation.teamstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamstation/teamstation/TeamstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamstation.teamstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

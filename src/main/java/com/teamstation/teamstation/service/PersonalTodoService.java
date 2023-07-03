1b470ec5fed7c103ebd4e015dc1106b1e1bb271b
jhsseonn <hs125bt@gmail.com>
2023-07-03 22:19:35 +0900

    fix: (Todo) todoDeadLine 타입 변경 및 생성자 수정

--- src/main/java/com/teamstation/teamstation/service/PersonalTodoService.java
+++ src/main/java/com/teamstation/teamstation/service/PersonalTodoService.java
@@ -1,13 +1,22 @@
 package com.teamstation.teamstation.service;
 
+import com.teamstation.teamstation.entity.PersonalTodo;
+import com.teamstation.teamstation.repository.MemberRepository;
+import com.teamstation.teamstation.repository.PersonalTodoRepository;
+import com.teamstation.teamstation.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PersonalTodoService {
 
+    private final TodoRepository todoRepository;
+    private final PersonalTodoRepository personalTodoRepository;
+    private final MemberRepository memberRepository;
 
 }

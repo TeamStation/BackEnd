818bf12f5b388275afc4261edc76cc5fb1b0213a
jhsseonn <hs125bt@gmail.com>
2023-07-03 21:55:14 +0900

    feat: entity, dto 생성 및 매핑 (#1)

---
+++ src/main/java/com/teamstation/teamstation/dto/PersonalTodoDto.java
@@ -0,0 +1,12 @@
+package com.teamstation.teamstation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PersonalTodoDto {
+
+    private Long personalTodoId;
+
+    private String category;
+}

eb3dc3b2b6aed6ef0362883639960cb2259add74
jhsseonn <hs125bt@gmail.com>
2023-07-06 22:57:55 +0900

    feat: TodoControllerTest 추가

---
+++ src/test/java/com/teamstation/teamstation/controller/TodoControllerTest.java
@@ -0,0 +1,13 @@
+package com.teamstation.teamstation.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoControllerTest {
+
+    @Test
+    void saveTodo() {
+
+    }
+}

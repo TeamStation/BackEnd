bb6a7db109524ff360a54dab2cce8543dc236a8a
jhsseonn <hs125bt@gmail.com>
2023-07-10 03:20:32 +0900

    feat: 프로젝트 생성 및 조회 로직 컨트롤러 추가 (#7)

--- src/main/java/com/teamstation/teamstation/controller/ProjectController.java
+++ src/main/java/com/teamstation/teamstation/controller/ProjectController.java
@@ -4,8 +4,10 @@ import com.teamstation.teamstation.dto.ProjectDto;
 import com.teamstation.teamstation.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,18 +22,45 @@ public class ProjectController {
 
     private ProjectService projectService;
 
-//    @PostMapping("/new")
-//    public @ResponseBody ResponseEntity saveProject(@RequestBody ProjectDto projectDto, BindingResult bindingResult, Principal principal) {
-//
-//    }
-//
-//    @GetMapping("/projects")
-//    public List<ProjectDto> getProjectList(Principal principal) {
-//
-//    }
-//
-//    @GetMapping("/projects/{projectId}")
-//    public ProjectDto getProjectDtl(@PathVariable("projectId") Long projectId){
-//
-//    }
+    @PostMapping("/new")
+    public @ResponseBody ResponseEntity saveProject(@RequestBody ProjectDto projectDto, BindingResult bindingResult, Principal principal) {
+        if(bindingResult.hasErrors()){
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for(FieldError fieldError: fieldErrors){
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        String email = principal.getName();
+        Long projectId;
+
+        try{
+            projectId = projectService.saveProject(projectDto, email);
+        }catch(Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Long>(projectId, HttpStatus.OK);
+    }
+
+    @GetMapping("/projects")
+    public List<Long> getProjectList(Principal principal) {
+        return projectService.getProjectIdList(principal.getName());
+    }
+
+    @GetMapping("/projects/{projectId}")
+    public ProjectDto getProjectDtl(@PathVariable("projectId") Long projectId){
+        return projectService.getProjectDtl(projectId);
+    }
+
+    @GetMapping("/projects/proceeding-count")
+    public int getProjectProceedingCount(Principal principal){
+        return projectService.getProceedingProjectSize(principal.getName());
+    }
+
+    @PostMapping("/projects/proceeding-count")
+    public int getProjectDoneCount(Principal principal){
+        return projectService.getDoneProjectSize(principal.getName());
+    }
 }
